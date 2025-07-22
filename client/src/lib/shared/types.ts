003b2aca8bc7fb027875b2bf1d4da2317acfcbf0
tolgatopkac-da <72669663+tolgatopkac@users.noreply.github.com>
2025-07-23 00:54:16 +0300

    property type

--- client/src/lib/shared/types.ts
+++ client/src/lib/shared/types.ts
@@ -11,3 +11,22 @@ export const CategorySchema = z.enum([
 	'Condo',
 	'Townhouse'
 ]);
+
+export const PropertySchema = z.object({
+	owner: z.string().regex(/^0x[a-fA-F0-9]{40}$/, 'Invalid Ethereum address'),
+	title: z.string().min(1, 'Title is required'),
+	description: z.string().min(1, 'Description is required'),
+	category: z.string(), // API'den gelen data CategorySchema enum'u match etmeyebilir
+	price: z.string().regex(/^\d+(\.\d+)?$/, 'Invalid price format'),
+	productId: z.number().nonnegative(),
+	reviewers: z.array(z.string()), // address[] reviewers
+	reviews: z.array(z.string()), // string[] reviews
+	image: z.string().url('Invalid image URL'),
+	address: z.string().min(1, 'Address is required'),
+	id: z.string(),
+	shortOwner: z
+		.string()
+		.regex(/^0x[a-fA-F0-9]{4}\.\.\.[a-fA-F0-9]{4}$/, 'Invalid short address format')
+});
+
+export type Property = z.infer<typeof PropertySchema>;
